18610492cee3edd0158aa1e6dfc9fa4dd3182492
jaclynflomen <j_floman@fanshaweonline.ca>
2019-04-21 00:40:45 -0400

    trying again

--- admin/scripts/editdelete.php
+++ admin/scripts/editdelete.php
@@ -28,11 +28,13 @@
 	// 	}
 	// }
 
-	function editProduct($img, $title, $brand, $color, $price){
+	function editProduct($id, $img, $title, $brand, $color, $price){
 		include('connect.php');
+
 		$update_product_query = 'UPDATE tbl_products SET product_img=:img, product_name=:title,';
 		$update_product_query .=' product_brand=:brand, product_color=:color, product_price=:price';
 		$update_product_query .=' WHERE product_id = :id';
+
 		$update_product_set = $pdo->prepare($update_product_query); 
 		$update_product_set->execute(
 			array(
@@ -44,6 +46,13 @@
 				':id'=>$id
 		)
 	);
+	if($update_product_set->rowCount()){
+		redirect_to('index.php');
+	}else{
+		//otherwise, return an error message
+		$message = 'Guess you got canned...';
+		return $message;
+	}
 	}
 
 	function deleteProduct($id){
