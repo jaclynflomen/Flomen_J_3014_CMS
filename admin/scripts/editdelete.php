d6ad631a5e2859bc8c96484b1b5455e8a12f57be
jaclynflomen <j_floman@fanshaweonline.ca>
2019-04-21 00:42:34 -0400

    trying again again

--- admin/scripts/editdelete.php
+++ admin/scripts/editdelete.php
@@ -1,33 +1,5 @@
 <?php 
 
-	// function editProduct($img, $title, $brand, $color, $price){
-	// 	include('connect.php');
-		
-	// 	$update_product_query = 'UPDATE tbl_products SET product_img=:img, product_name=:title,';
-	// 	$update_product_query .=' product_brand=:brand, product_color=:color, product_price=:price';
-	// 	$update_product_query .=' WHERE product_id = :id';
-
-	// 	$update_product_set = $pdo->prepare($update_product_query);
-	// 	$update_product_set->execute(
-	// 		array(
-	// 			':img'=>$img,
-	// 			':name'=>$title,
-	// 			':brand'=>$brand,
-	// 			':color'=>$color,
-	// 			':price'=>$price,
-	// 			':id'=>$id
-	// 		)
-	// 	);
-	// 	//When update successfully, redirect product to index.php
-	// 	if($update_product_set->rowCount()){
-	// 		redirect_to('index.php');
-	// 	}else{
-	// 		//otherwise, return an error message
-	// 		$message = 'Guess you got canned...';
-	// 		return $message;
-	// 	}
-	// }
-
 	function editProduct($id, $img, $title, $brand, $color, $price){
 		include('connect.php');
 
